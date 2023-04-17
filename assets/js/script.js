08ff3a5cadc0afe6aa1258f17d68c55d108669ae
RichardButurla <rj.buturla@gmail.com>
2023-04-17 21:46:59 +0100

    setup moving player and screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,74 @@
 const canvas = document.getElementById("the_canvas");
 const context = canvas.getContext("2d");
 
-import('./menu.js')
-  .then((module) => {
-    const menu = new module.Menu();
-    menu.update();
-  })
-  .catch((error) => {
-    console.error(error);
+const screenStates = Object.freeze({ 
+    MenuState: 0,
+    GamePlayState: 1,
+    InventoryState: 2,
   });
 
-const menuScreen = new Menu();
+let currentScreenState = screenStates.GamePlayState;
+
+class MenuScreen
+{
+    update()
+    {
+        //console.log("Update from Menu");
+    };
+    draw()
+    {
+        context.font = "80px serif";
+        context.fillText("MenuScreen",context.canvas.width - (context.canvas.width / 2), context.canvas.height - (context.canvas.height / 2));
+    };
+}
+
+class GamePlayScreen
+{
+    update()
+    {
+        if (gamerInput.action === "Up") {
+            console.log("Move Up");
+            player.y -= 5; // Move Player Up      
+        } 
+        if (gamerInput.action === "Down") {
+            console.log("Move Down");
+            player.y += 5; // Move Player Down
+        } 
+         if (gamerInput.action === "Left") {
+            console.log("Move Left");
+            player.x -= 5; // Move Player Left
+        } 
+         if (gamerInput.action === "Right") {
+            console.log("Move Right");
+            player.x += 5; // Move Player Right
+        }
+
+        player.x += playerMoveVector.x;
+        player.y -= playerMoveVector.y;
+    };
+    draw()
+    {
+        context.drawImage(backgroundImg,0,0,context.canvas.width,context.canvas.height);
+        context.drawImage(player.spritesheet,player.x,player.y);
+    };
+}
+class InventoryScreen
+{
+    update()
+    {
+        //console.log("Update from Inventory");
+    };
+    draw()
+    {
+
+    };
+}
+
+const menuScreen = new MenuScreen();
+const gameplayScreen = new GamePlayScreen();
+const inventoryScreen = new InventoryScreen();
+
+
 
 var options = {
     mode: 'static',
@@ -22,6 +80,11 @@ var options = {
 
 let backgroundImg = new Image();
 backgroundImg.src = "assets/img/canvas-background.png";
+
+let playerImg = new Image();
+playerImg.src = "assets/img/detective.png";
+
+let player = new GameObject(playerImg, 300, 100, 100, 150);
 
 function GameObject(spritesheet, x, y, width, height) {
     this.spritesheet = spritesheet;
@@ -96,16 +159,20 @@ function input(event) {
         if (event.type === "keydown") {
         switch (event.keyCode) {
             case 65: // A Key// blue
-                blueButton.classList.add("pressed");
+                //blueButton.classList.add("pressed");
+                gamerInput = new GamerInput("Left");  
                 break; 
             case 87: // W Key // yellow
-                yellowButton.classList.add("pressed");            
+                //yellowButton.classList.add("pressed"); 
+                gamerInput = new GamerInput("Up");           
                 break; 
             case 68: // D Key // red
-                redButton.classList.add("pressed");   
+                //redButton.classList.add("pressed");
+                gamerInput = new GamerInput("Right");     
                 break; 
             case 83: // S Key // green
-                greenButton.classList.add("pressed");
+                //qgreenButton.classList.add("pressed");
+                gamerInput = new GamerInput("Down");  
                 break; 
 
             default:
@@ -123,40 +190,39 @@ function input(event) {
 
 
 function update() {
-
-    menuScreen.update();
-    // console.log("Update");
-    // Check Input
-    if (gamerInput.action === "Up") {
-        console.log("Move Up");
-        player.y -= 5; // Move Player Up      
-    } 
-    if (gamerInput.action === "Down") {
-        console.log("Move Down");
-        player.y += 5; // Move Player Down
-    } 
-     if (gamerInput.action === "Left") {
-        console.log("Move Left");
-        player.x -= 5; // Move Player Left
-    } 
-     if (gamerInput.action === "Right") {
-        console.log("Move Right");
-        player.x += 5; // Move Player Right
-    }
-             
-    //update other objects
-    checkBounds();
+    switch(currentScreenState)
+    {
+        case screenStates.MenuState:
+            menuScreen.update();
+            break;
+        case screenStates.GamePlayState:
+            gameplayScreen.update();
+            break;
+        case screenStates.InventoryState:
+            inventoryScreen.update();
+            break;
+    }        
 }
 
 function draw() {
     context.clearRect(0,0, canvas.width, canvas.height);
-    menuScreen.draw()
-   
+
+    switch(currentScreenState)
+    {
+        case screenStates.MenuState:
+            menuScreen.draw();
+            break;
+        case screenStates.GamePlayState:
+            gameplayScreen.draw();
+            break;
+        case screenStates.InventoryState:
+            inventoryScreen.draw();
+            break;
+    }
 }
 
 function gameloop() {
     update();
-    animate();
     draw();
     
     window.requestAnimationFrame(gameloop);

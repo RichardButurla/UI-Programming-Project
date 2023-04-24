e6e6efea5f0841a2c2bef39399ad058e6d6cf664
RichardButurla <rj.buturla@gmail.com>
2023-04-24 14:44:34 +0100

    checks mobile/desktop

--- assets/js/initial.js
+++ assets/js/initial.js
@@ -1,3 +1,16 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    console.log("mobile");
+    // Code to execute if the website is being accessed from a mobile device
+  } else {
+    // Code to execute if the website is being accessed from a desktop device
+    console.log("desktop");
+    const dPad = document.getElementById("dPad");
+    const joyStickArea = document.getElementById("joystick-area");
+    joyStickArea.style.display = "none";
+    dPad.style.display = "none";
+  }
+
+
 if(typeof(Storage) !== "undefined") {
    
     // console.log("Local storage is supported.");
